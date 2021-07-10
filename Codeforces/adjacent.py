0efc498bbac77458fef9d780e87aa84fb14e33ed
Rupesh Chandra Mohanty <rupeshmohanty67@gmail.com>
2021-07-10 16:44:15 +0530

    Adjacent replacements program modified

--- Codeforces/adjacent.py
+++ Codeforces/adjacent.py
@@ -1,12 +1,8 @@
 n = int(input())
 a = list(map(int, input().split()))
-s = " ".join(str(e) for e in a)
 
-for i in range(len(s)):
-    if s[i] != ' ':
-        if int(s[i]) % 2 == 0:
-            s = s.replace(s[i], str(int(s[i]) - 1))
-        else:
-            s = s.replace(s[i], str(int(s[i]) + 1))
-    
-print(s)
+for i in range(n):
+    if a[i] % 2 == 0:
+        a[i] = a[i] - 1
+
+print(" ".join(str(e) for e in a))
